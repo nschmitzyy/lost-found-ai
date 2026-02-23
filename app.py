9a3d4aa1638fc591ee6ef46e47f899553e29e1fc
nschmitzyy <schmitzniklas08@gmail.com>
2026-02-23 15:06:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+st.set_page_config(page_title="AI Lost & Found", layout="wide")
+
+st.title("🔎 AI Lost & Found System")
+
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Report Lost", "Report Found", "Matches"])
+
+if page == "Report Lost":
+    st.switch_page("pages/report_lost.py")
+
+if page == "Report Found":
+    st.switch_page("pages/report_found.py")
+
+if page == "Matches":
+    st.switch_page("pages/matches.py")
