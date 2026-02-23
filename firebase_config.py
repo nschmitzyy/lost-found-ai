649af8ed0f35a95bfeaa97add825f534ed74b59b
nschmitzyy <schmitzniklas08@gmail.com>
2026-02-23 15:06:43 +0100

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,1 +1,14 @@
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
+import streamlit as st
+import json
 
+if not firebase_admin._apps:
+    firebase_dict = json.loads(st.secrets["FIREBASE_CREDENTIALS"])
+    cred = credentials.Certificate(firebase_dict)
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': st.secrets["FIREBASE_STORAGE_BUCKET"]
+    })
+
+db = firestore.client()
+bucket = storage.bucket()
